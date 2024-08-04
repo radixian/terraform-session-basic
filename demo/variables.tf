115c99f1b4b8e9c376486664cfc5022d3cfb0aeb
Ravindu Dolawatta <63357768+radixian@users.noreply.github.com>
2024-08-04 21:00:55 +0100

    Create variables.tf

---
+++ demo/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+ default = "us-east-1" # North Virginia region
+}
+variable "vpc_cidr" {
+ default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+ default = "10.0.1.0/24"
+}
+variable "private_subnet_cidr" {
+ default = "10.0.2.0/24"
+}
+variable "instance_type" {
+ default = "t2.micro"
+}
+variable "db_instance_class" {
+ default = "db.t3.micro"
+}
+variable "db_name" {
+ default = "sampledb"
+}
