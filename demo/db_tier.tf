823069835fdea35f2c364e73869f3dcb418a56fe
Ravindu Dolawatta <63357768+radixian@users.noreply.github.com>
2024-08-04 21:01:55 +0100

    Create db_tier.tf

---
+++ demo/db_tier.tf
@@ -0,0 +1,44 @@
+resource "aws_db_subnet_group" "demo_db_subnet_group" {
+ name       = "demo-db-subnet-group"
+ subnet_ids = [aws_subnet.demo_pvt_subnet.id]
+ tags = {
+   Name = "demo-db-subnet-group"
+ }
+}
+resource "aws_rds_cluster" "demo_db_cluster" {
+ cluster_identifier      = "demo-db-cluster"
+ engine                  = "aurora-mysql"
+ master_username         = var.db_username
+ master_password         = var.db_password
+ skip_final_snapshot     = true
+ db_subnet_group_name    = aws_db_subnet_group.demo_db_subnet_group.id
+ vpc_security_group_ids  = [aws_security_group.demo_db_sg.id]
+ lifecycle {
+   ignore_changes = [tags]
+ }
+ tags = {
+   Name = "demo-db-cluster"
+ }
+}
+resource "aws_rds_cluster_instance" "demo_db_instance" {
+ count              = 1
+ identifier         = "demo-db-instance-${count.index}"
+ cluster_identifier = aws_rds_cluster.demo_db_cluster.id
+ instance_class     = var.db_instance_class
+ tags = {
+   Name = "demo-db-instance"
+ }
+}
+resource "null_resource" "demo_db_setup" {
+ depends_on = [aws_rds_cluster.demo_db_cluster]
+ provisioner "local-exec" {
+   command = <<-EOT
+     curl -o mysqlsampledatabase.sql
+https://raw.githubusercontent.com/hhorak/mysql-sample-db/master/mysqlsampledatabase.sql
+     mysql -h ${aws_rds_cluster.demo_db_cluster.endpoint} -u ${var.db_username} -p${var.db_password} ${var.db_name} < mysqlsampledatabase.sql
+   EOT
+ }
+}
+output "db_endpoint" {
+ value = aws_rds_cluster.demo_db_cluster.endpoint
+}
