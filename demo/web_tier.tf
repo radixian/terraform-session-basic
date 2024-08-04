d1547fbdeb9c542fbc0a613b37fde688c539de65
Ravindu Dolawatta <63357768+radixian@users.noreply.github.com>
2024-08-04 21:01:20 +0100

    Create web_tier.tf

---
+++ demo/web_tier.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "demo_web" {
+ ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+ instance_type = var.instance_type
+ subnet_id     = aws_subnet.demo_plc_subnet.id
+ security_groups = [aws_security_group.demo_web_sg.name]
+ user_data = <<-EOF
+             #!/bin/bash
+             yum update -y
+             yum install -y httpd
+             systemctl start httpd
+             systemctl enable httpd
+             echo "Hello, World from $(hostname -f)" > /var/www/html/index.html
+             EOF
+ tags = {
+   Name = "demo-web-instance"
+ }
+}
+output "web_instance_public_ip" {
+ value = aws_instance.demo_web.public_ip
+}
