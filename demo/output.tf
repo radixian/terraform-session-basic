65ce4f5088d453e5e3a04b82d234780b29d0df92
Ravindu Dolawatta <63357768+radixian@users.noreply.github.com>
2024-08-04 21:20:14 +0100

    Update output.tf

--- demo/output.tf
+++ demo/output.tf
@@ -1,6 +1,7 @@
 output "web_instance_public_ip" {
- value = aws_instance.web.public_ip
+ value = aws_instance.demo_web.public_ip
 }
+
 output "db_endpoint" {
- value = aws_rds_cluster.aurora.endpoint
+ value = aws_rds_cluster.demo_db_cluster.endpoint
 }
